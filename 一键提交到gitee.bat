9a6188865a3e864de05ebc53b6fd825c628f8fb9
wuyuanwuhui99 <275018723@qq.com>
2025-09-07 20:18:56 +0800

    修改项目配置

--- 一键提交到gitee.bat
+++ 一键提交到gitee.bat
@@ -1,4 +1,4 @@
 git remote rm origin
-git remote add origin https://gitee.com/wuyuanwuhui99/flutter-music-chat-ui
+git remote add origin https://gitee.com/wuyuanwuhui99/flutter-chat-app-ui
 git push origin main
 pause
--- 一键提交到github.bat
+++ 一键提交到github.bat
@@ -1,4 +1,4 @@
 git remote rm origin
-git remote add origin https://github.com/wuyuanwuhui999/flutter-music-chat-ui
+git remote add origin https://github.com/wuyuanwuhui999/flutter-chat-app-ui
 git push origin main
 pause
