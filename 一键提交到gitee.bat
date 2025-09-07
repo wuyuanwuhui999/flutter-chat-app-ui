9a3118416df3812c9025ee01d4cc768ca4110a06
wuyuanwuhui99 <275018723@qq.com>
2025-09-07 20:21:46 +0800

    修改代码提交命令

--- 一键提交到gitee.bat
+++ 一键提交到gitee.bat
@@ -1,4 +1,4 @@
 git remote rm origin
 git remote add origin https://gitee.com/wuyuanwuhui99/flutter-chat-app-ui
-git push origin main
+git push origin master
 pause
--- 一键提交到github.bat
+++ 一键提交到github.bat
@@ -1,4 +1,4 @@
 git remote rm origin
 git remote add origin https://github.com/wuyuanwuhui999/flutter-chat-app-ui
-git push origin main
+git push origin master
 pause
